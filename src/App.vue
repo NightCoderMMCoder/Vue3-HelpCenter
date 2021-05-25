e2eaa91de3a577a8bcee22fb57143168276ca5c5
aungmyat12 <aungmyatoo2580@gmail.com>
2021-05-26 00:19:12 +0630

    2-Navbar

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Navbar />
+  <router-view />
+</template>
+
+<script>
+import Navbar from "./components/Layout/Navbar.vue";
+export default {
+  components: { Navbar },
+  setup() {},
+};
+</script>
---
+++ src/components/Layout/Navbar.vue
@@ -0,0 +1,93 @@
+<template>
+  <nav>
+    <div class="container">
+      <div class="navbar">
+        <h1><span>Help </span> Center</h1>
+      </div>
+      <input
+        type="text"
+        placeholder="Search By Supports"
+        class="form-control"
+      />
+      <div>
+        <div class="btn-groups">
+          <button class="btn btn-secondary">Login</button>
+          <button class="btn btn-secondary">Register</button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  padding: 1rem 0;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .navbar {
+      display: flex;
+      align-items: center;
+    }
+    span {
+      color: var(--secondary);
+    }
+    .login {
+      margin-right: 15px;
+    }
+    .username {
+      margin-right: 15px;
+    }
+    small {
+      margin-right: 15px;
+    }
+  }
+  background: var(--primary);
+  color: white;
+  ul {
+    margin-left: 20px;
+    display: flex;
+    list-style: none;
+    li {
+      display: inline-block;
+      font-size: 17px;
+      margin: 0 15px;
+      color: var(--secondary);
+      position: relative;
+    }
+  }
+  .form-control {
+    padding: 10px;
+    border: 1px solid #00000028;
+    outline: none;
+    border-radius: 5px;
+    max-width: 300px;
+    width: 250px;
+    &:focus {
+      box-shadow: 0 0 0 0.1rem #ffcd9498;
+    }
+  }
+  .router-link-exact-active li::after {
+    content: "";
+    position: absolute;
+    border-bottom: 3px solid var(--secondary);
+    width: 0%;
+    left: 0;
+    bottom: -7px;
+    animation: slider 0.5s linear forwards;
+  }
+  @keyframes slider {
+    from {
+      width: 0;
+    }
+    to {
+      width: 100%;
+    }
+  }
+}
+</style>
